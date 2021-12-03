e2966f0cef8ac2dd80fc1afccc7e1ee7a8e72951
Louis Pinsard <louispinsard@gmail.com>
2021-12-03 16:17:18 +0100

    fix pynamodb model

--- runtime/chalicelib/models/virus.py
+++ runtime/chalicelib/models/virus.py
@@ -5,6 +5,7 @@ from pynamodb.attributes import UnicodeAttribute
 class VirusModel(Model):
     class Meta:
         table_name = os.environ.get("APP_TABLE_NAME", "")
+        region = 'eu-west-1'
 
-    modelName = UnicodeAttribute(default='Virus', attr_name="PK")
+    modelName = UnicodeAttribute(default='Virus', attr_name="PK", hash_key=True)
     uuid = UnicodeAttribute(range_key=True, attr_name="SK")
